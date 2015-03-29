e21944b104664c2f34a8abc957a68e29f7075283
Myroslav Lisniak <miroslav_l@playtech.com>
2015-03-29 15:55:42 +0300

    divide data and logic

---
+++ src/portfolio/frontend-nanodegree-resume/js/jsonContent.js
@@ -0,0 +1,95 @@
+/*work contains an array of jobs. Each job object in jobs should contain an employer, title, 
+ * location, dates worked and description.
+ * var work = {};
+	work.current_position = "cost accountant";
+	work.employer = "Ciklum";
+	work.years_worked =  2;
+	work.city = "Kyev";
+*/
+
+var work = { 
+	"jobs" : [
+		{
+		 "employer" : "Ciklum",
+		 "title" : "web developer",
+		 "location" : "Kyiv",
+		 "dates_worked" : "November 2012 - now",
+		 "description" : "description"	
+			},
+		{
+		 "employer" : "Yuna Company",
+		 "title" : "web developer",
+		 "location" : "Kyiv",
+		 "dates_worked" : "April 2011 - November 2012",
+		 "description" : "description"	
+			}
+	]
+}
+/*
+projects con tains an array of projects. Each project object in projects should contain a title, 
+* dates worked, description, and an images array with URL strings for project images.
+
+bio contains a name, role, welcomeMessage, contacts object and skills array. 
+* The contacts object should contain (but doesn't have to) a mobile number,
+*  email address, github username, twitter handle and location.
+
+education contains an array of schools. Each school object in schools contains a name, 
+* location, degree, majors array, dates attended and a url for the school's website. 
+* education also contains an onlineCourses array. Each onlineCourse object in onlineCourses 
+* should contain a title, school, dates attended and a url for the course.
+
+*/
+
+var bio = {
+	"name": "Nadia Daliavska",
+	"role": "Web Developer",
+	"contact_info": {
+	"mobile" : "0937160335",
+	"email" : "nadiadaliavska@gmail.com",
+	"github" : "NadiaDal",
+	"location" : "Kyiv"
+},
+	"welcome_message" : "Hi!",
+	"skills" : ["html", "javascript", "css", "bootsrap"],
+	"pic" : "images/fry.jpg"
+	}
+	
+
+var education = {};
+	education["school_name"] = "Acadamy of Acconting and Statictic";
+	education["school_attended"] = 4;
+	education["school_city"]= "Bobrynets";
+
+var edu = {
+    "schools": [
+        {
+            "name": "Academy of Statistics and Accounting",
+            "location": "Kyev",
+            "degree": "master",
+            "majors": [
+                "",
+                ""
+            ],
+            "dates": "",
+            "url": ""
+        },
+        {
+            "name": "",
+            "location": "",
+            "degree": "",
+            "majors": [
+                "",
+                ""
+            ],
+            "dates": "",
+            "url": ""
+        },
+        {
+            "online_courses": [
+                "udacity",
+                "coursera",
+                "codefire"
+            ]
+        }
+    ]
+}
--- src/portfolio/frontend-nanodegree-resume/js/resumeBuilder.js
+++ src/portfolio/frontend-nanodegree-resume/js/resumeBuilder.js
@@ -6,67 +6,46 @@ console.log(awesomeThoughts);
 var funThoughts = awesomeThoughts.replace("Awesome","Fun");
 $("#main").append(funThoughts);
 */
-
-var skills =
-["html", "javascript", "css", "bootsrap"];
-
-var contacts = {
-	"mobile" : "0937160335",
-	"email" : "nadiadaliavska@gmail.com",
-	"github" : "NadiaDal",
-	"location" : "Kyiv"
-}
-
-var bio = {
-	"name": "Nadia Daliavska",
-	"role": "Web Developer",
-	"contact_info": contacts,
-	"welcome_message" : "Hi!",
-	"skills" : skills,
-	"pic" : "images/fry.jpg"
-	}
 	
-var work = {};
-	work.current_position = "cost accountant";
-	work.employer = "Ciklum";
-	work.years_worked =  2;
-	work.city = "Kyev";
-
-
-var education = {};
-	education["school_name"] = "Acadamy of Acconting and Statictic";
-	education["school_attended"] = 4;
-	education["school_city"]= "Bobrynets";
-
-	
-var formattedRole = HTMLheaderName.replace("%data%", bio.role);
+/*var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedMobile = HTMLmobile.replace("%data%", bio.contact_info.mobile);
 var formattedEmail = HTMLemail.replace("%data%", bio.contact_info.email);
 var formattedGithub = HTMLgithub.replace("%data%", bio.contact_info.github);
 var formattedLocation = HTMLlocation.replace("%data%", bio.contact_info.location);
 var formattedPic = HTMLbioPic.replace("%data%", bio.pic);
-
 var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 var formattedWM = HTMLwelcomeMsg.replace("%data%", bio.welcome_message);
 var linebreak = document.createElement("br");
-
-
 $("#header").append(formattedRole);
-$("#header").prepend(formattedName);
-$("#header").append(formattedMobile);
-$("#header").append(formattedEmail);
-$("#header").append(formattedGithub);
-$("#header").append(formattedLocation);
-$("#header").append(formattedPic);
-$("#header").append(formattedWM);
-$("#header").append(HTMLskillsStart);
-$("#header").append(formattedSkills);
+*/
 
-//var formattedScholl_name = HTMLschoolName.replace("%data%", );
+$("#header").append(HTMLheaderName.replace("%data%", bio.name));
+$("#header").append(HTMLheaderRole.replace("%data%", bio.role));
+
+$("#header").append(HTMLmobile.replace("%data%", bio.contact_info.mobile));
+$("#header").append(HTMLemail.replace("%data%", bio.contact_info.email));
+$("#header").append(HTMLgithub.replace("%data%", bio.contact_info.github));
+$("#header").append(HTMLlocation.replace("%data%", bio.contact_info.location));
+$("#header").append(HTMLbioPic.replace("%data%", bio.pic));
+$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcome_message));
+
+if(bio.skills.length !=0){
+
+	var formattedSkillsStart = HTMLskillsStart.replace("%data%", bio.skills);
+	$("#header").append(formattedSkillsStart);
+	$("#skills").append(formattedSkills);
+	
+}
+
+/*
+ * $("#header").append(formattedSkillsStart);
+$("#skills").append(formattedSkills);
+
+/*var formattedScholl_name = HTMLschoolName.replace("%data%", );
 $("#header").append(education["school_name"]);
 $("#header").append(work.current_position);
-
+*/
 
 
 
